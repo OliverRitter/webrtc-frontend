4c916f4e139fce1d56eaa3a761e66348e477d126
ol Rit <ol.ritte@web.de>
2022-04-10 21:02:55 +0200

    React 18

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import App from './App';
 
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
